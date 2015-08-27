2280bba19d0386c94bd55ad6be56b443f517eee3
Jimmy <jimmy_man_7@hotmail.com>
2015-08-27 15:31:57 +0200

    made model WorkItem

---
+++ case-managment-service/src/main/java/se/majp/caseManagement/model/WorkItem.java
@@ -0,0 +1,64 @@
+package se.majp.caseManagement.model;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "workitem")
+public class WorkItem {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Column(name = "workItemId")
+	private int workItemId;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "priority")
+	private Priority priority;
+
+	@Column(name = "status")
+	private Status status;
+
+	@ManyToMany
+	private Collection<User> users;
+
+	@OneToMany
+	private Collection<Issue> issues;
+
+	@ManyToMany
+	private Project project;
+
+	protected WorkItem() {
+	}
+
+	public WorkItem(int workItemId, String description, Priority priority, Status status) {
+
+		this.workItemId = workItemId;
+		this.description = description;
+		this.priority = priority;
+		this.status = status;
+
+	}
+
+	enum Priority {
+		VERY_HIGH, HIGH, NORMAL, LOW, VERY_LOW
+	}
+
+	enum Status {
+		// add status
+		// example
+		INPROGRESS, DONE, TEST
+	}
+
+}
