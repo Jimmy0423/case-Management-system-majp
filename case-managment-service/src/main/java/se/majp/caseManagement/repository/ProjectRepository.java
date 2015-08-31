344c5e13b3c725d8bb66d8dff424a61280cc3d0f
Marcus Svensson <svensson.marcus.9@gmail.com>
2015-08-31 12:57:48 +0200

    Added project repo

---
+++ case-managment-service/src/main/java/se/majp/caseManagement/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package se.majp.caseManagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import se.majp.caseManagement.model.Project;
+
+public interface ProjectRepository extends CrudRepository<Project, Long>
+{
+
+}
