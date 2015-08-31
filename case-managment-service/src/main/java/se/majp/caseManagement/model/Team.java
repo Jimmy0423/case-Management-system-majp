c92a99b6b4d5c389c5e6d449248af654da0f71bb
Patrik Appelqvist <Appelqvist.patrik@gmail.com>
2015-08-31 13:08:11 +0200

    added hashcode and equals

--- case-managment-service/src/main/java/se/majp/caseManagement/model/Team.java
+++ case-managment-service/src/main/java/se/majp/caseManagement/model/Team.java
@@ -11,11 +11,28 @@ import javax.persistence.OneToMany;
 @Entity
 public class Team extends AbstractEntity
 {
+	private String teamId;
+	
 	@OneToMany
 	@JoinTable(name = "member_in_team")
 	private final Collection<TeamMember> members = new ArrayList<>();
 
 	protected Team(){}
+	
+	public Team(String teamId)
+	{
+		this.teamId = teamId;
+	}
+	
+	public String getTeamId()
+	{
+		return teamId;
+	}
+	
+	public Collection<TeamMember> getMembers()
+	{
+		return members;
+	}
 
 	public Team addUser(TeamMember member)
 	{
@@ -36,5 +53,28 @@ public class Team extends AbstractEntity
 	public void removeUser(TeamMember member)
 	{
 		members.remove(member);
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result += prime * teamId.hashCode();
+		result += prime * members.hashCode();
+		
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj instanceof Team)
+		{
+			Team other = (Team) obj;
+			return other.getTeamId().equals(getTeamId()) && other.getMembers().equals(getMembers());
+		}
+		
+		return false;
 	}
 }
--- case-managment-service/src/main/java/se/majp/caseManagement/model/TeamMember.java
+++ case-managment-service/src/main/java/se/majp/caseManagement/model/TeamMember.java
@@ -37,6 +37,29 @@ public class TeamMember extends AbstractEntity
 		return role;
 	}
 	
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result += prime * user.hashCode();
+		
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj instanceof TeamMember)
+		{
+			TeamMember other = (TeamMember) obj;
+			
+			return other.getUser().equals(getUser());
+		}
+		
+		return false;
+	}
+	
 	public enum Role
 	{
 		MEMBER, OWNER
