b1ba0e480b31a2b150b877e4804433d5ef5ac188
Aymen Arbi <aymen.arbi@gmail.com>
2015-08-31 13:48:31 +0200

    implemented findBy methods in workItemRepo

--- case-managment-service/src/main/java/se/majp/caseManagement/repository/WorkItemRepository.java
+++ case-managment-service/src/main/java/se/majp/caseManagement/repository/WorkItemRepository.java
@@ -2,8 +2,10 @@ package se.majp.caseManagement.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import se.majp.caseManagement.model.Team;
 import se.majp.caseManagement.model.WorkItem;
 import se.majp.caseManagement.model.WorkItem.Status;
 
@@ -11,5 +13,8 @@ public interface WorkItemRepository extends CrudRepository<WorkItem, Long>
 {
 	List<WorkItem> findByStatus(Status status);
 
-	List<WorkItem> findByDescription(String description);
+	@Query("select w from tbl_work_item w where w.project.team = ?1")
+	List<WorkItem> findByTeam(Team team);
+
+	List<WorkItem> findByDescriptionContain(String description);
 }
