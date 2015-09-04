6edb7e17cf8d44743af8d121a5e0d3f835d74a44
Marcus Svensson <svensson.marcus.9@gmail.com>
2015-09-04 16:29:55 +0200

    Tried some other dependencies

--- case-management-service/src/main/java/se/majp/caseManagement/service/UserService.java
+++ case-management-service/src/main/java/se/majp/caseManagement/service/UserService.java
@@ -1,10 +1,12 @@
 package se.majp.caseManagement.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import se.majp.caseManagement.model.User;
 import se.majp.caseManagement.repository.UserRepository;
 
+@Service
 public class UserService
 {
 	@Autowired
