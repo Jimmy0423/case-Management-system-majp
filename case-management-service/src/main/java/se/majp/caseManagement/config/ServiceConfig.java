98eb60d46178dd27c9bd2b3227a820b30b831d5a
Patrik Appelqvist <Appelqvist.patrik@gmail.com>
2015-09-08 22:31:32 +0200

    removed scope from config

--- case-management-service/src/main/java/se/majp/caseManagement/config/ServiceConfig.java
+++ case-management-service/src/main/java/se/majp/caseManagement/config/ServiceConfig.java
@@ -2,7 +2,6 @@ package se.majp.caseManagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import se.majp.caseManagement.service.ProjectService;
@@ -14,21 +13,18 @@ import se.majp.caseManagement.service.UserService;
 public class ServiceConfig
 {
 	@Bean
-	@Scope("request")
 	public UserService userService()
 	{
 		return new UserService();
 	}
 
 	@Bean
-	@Scope("request")
 	public ProjectService projectService()
 	{
 		return new ProjectService();
 	}
 	
 	@Bean
-	@Scope("request")
 	public StoryService storyService()
 	{
 		return new StoryService();
