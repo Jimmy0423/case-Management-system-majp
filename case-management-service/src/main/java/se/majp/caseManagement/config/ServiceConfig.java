9b74036e00c174f4aeba7632088c7642bb8cc305
Patrik Appelqvist <Appelqvist.patrik@gmail.com>
2015-09-08 17:36:27 +0200

    fix with integration

--- case-management-service/src/main/java/se/majp/caseManagement/config/ServiceConfig.java
+++ case-management-service/src/main/java/se/majp/caseManagement/config/ServiceConfig.java
@@ -2,10 +2,12 @@ package se.majp.caseManagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import se.majp.caseManagement.service.UserService;
 
 @Configuration
+@EnableJpaRepositories("se.majp.caseManagement.repository")
 public class ServiceConfig
 {
 	@Bean
