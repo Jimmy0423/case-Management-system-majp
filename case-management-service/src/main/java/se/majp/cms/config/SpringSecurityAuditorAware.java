47252b18872770acf1e0ecd33c69bedef02061c7
Marcus Svensson <svensson.marcus.9@gmail.com>
2015-09-17 22:39:57 +0200

    started implementing auditoraware

---
+++ case-management-service/src/main/java/se/majp/cms/config/SpringSecurityAuditorAware.java
@@ -0,0 +1,25 @@
+package se.majp.cms.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import se.majp.cms.model.User;
+
+public class SpringSecurityAuditorAware implements AuditorAware<String>
+{
+
+	@Override
+	public String getCurrentAuditor()
+	{
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (authentication == null || !authentication.isAuthenticated())
+		{
+			return null;
+		}
+		
+		return ((User) authentication.getPrincipal()).getUserId();
+	}
+
+}
